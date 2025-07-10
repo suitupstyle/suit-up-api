4adae072485555b71b78d0f9af154424a16b6262
Armando <arman2p284@gmail.com>
2025-07-10 11:14:12 -0400

    feat: adds example of use for the excel generation queue

--- src/modules/orders/controllers/orders.controller.ts
+++ src/modules/orders/controllers/orders.controller.ts
@@ -3,6 +3,8 @@ import { HttpError } from '../../../utils/error'
 import { SuccessResponse } from '../../../utils/response'
 import { Order } from '../entities/order'
 import { OrderService } from '../services/orders.service'
+import { ExcelGenerationJob } from '../../../types/definitions'
+import { excelQueue } from '../queues/excel.queue'
 
 const service = new OrderService()
 
@@ -20,6 +22,28 @@ export const createOrder: RequestHandler = async (
     if (err instanceof HttpError) {
       res.status(err.status).json({ error: { message: err.message } })
     }
+    next(err)
+  }
+}
+
+/**
+ * Example method of Excel generation using the queue system
+ */
+export const generateExcelExample: RequestHandler = async (req, res, next) => {
+  const job: ExcelGenerationJob = {
+    templatePath: './templates/report.xlsx',
+    outputPath: `./output/report-${Date.now()}.xlsx`,
+    updates: [], // <-- Here goes the data to update, just check the type definition
+    metadata: {} // <-- Context additional data, optional
+  };
+
+  try {
+    await excelQueue.addJob(job);
+    res.json({
+      success: true,
+      queueStatus: excelQueue.getStatus() // <-- You can make a stream for this, or an endpoint to check it regularly
+    });
+  } catch (err) {
     next(err)
   }
 }
