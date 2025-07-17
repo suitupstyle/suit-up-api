e90624a35ca7d2092462f1e2bb5716624d4ced85
Reynier <reynierp.salas@gmail.com>
2025-07-16 22:32:24 -0400

    docs: add docs for payment endpoints

--- src/modules/payments/routes/payments.routes.ts
+++ src/modules/payments/routes/payments.routes.ts
@@ -3,6 +3,38 @@ import { createPaymentIntent } from '../controllers/payments.controller'
 
 const router = Router()
 
+/**
+ * @openapi
+ * /payments/create-intent:
+ *   post:
+ *     summary: Create a new Stripe PaymentIntent
+ *     tags:
+ *       - Payments
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreatePaymentIntentInput'
+ *     responses:
+ *       '201':
+ *         description: The PaymentIntent was created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     clientSecret:
+ *                       type: string
+ *                       description: The client_secret to confirm the payment on the frontend.
+ *       '422':
+ *         $ref: '#/components/responses/ErrorResponse'
+ *       '500':
+ *         $ref: '#/components/responses/ErrorResponse'
+ */
 router.post('/create-intent', createPaymentIntent)
 
 export default router
